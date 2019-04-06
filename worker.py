6ea1a5da92f14c7b271ab6686a1145fa07d2f4b9
Felarm <kanafjev@gmail.com>
2019-04-06 18:30:52 +0300

    добавил docker-compose.yml для запуска через docker-compose up

--- worker.py
+++ worker.py
@@ -2,7 +2,7 @@ import requests, redis
 from rq import Worker, Queue, Connection
 
 listen = ['default']
-redis_url = 'redis://localhost:6379'
+redis_url = 'redis://redis:6379/0'
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
